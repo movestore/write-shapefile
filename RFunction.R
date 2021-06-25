be3825a35619d5802d4f8fdbb8e8db4155e39aab
Andrea Kölzsch <65662928+andreakoelzsch@users.noreply.github.com>
2021-06-25 12:18:17 +0200

    Update RFunction.R

--- RFunction.R
+++ RFunction.R
@@ -6,7 +6,8 @@ rFunction <- function(data)
   
   data.spdf <- SpatialPointsDataFrame(coordinates(data),as.data.frame(data), proj4string=CRS("+proj=longlat +ellps=WGS84 +no_defs"))
   
-  writeOGR(data.spdf,dsn=paste0(Sys.getenv(x = "APP_ARTIFACTS_DIR", "/tmp/"),"shapefile_output"),layer="data",driver="ESRI Shapefile") #ask Clemens for path
+  writeOGR(data.spdf,dsn=Sys.getenv(x = "APP_ARTIFACTS_DIR", "/tmp/"),layer="data",driver="ESRI Shapefile") 
+  #writeOGR(data.spdf,dsn=paste0(Sys.getenv(x = "APP_ARTIFACTS_DIR", "/tmp/"),"shapefile_output"),layer="data",driver="ESRI Shapefile") #ask Clemens for path
   
   #writeOGR(data.spdf,dsn="shapefile_output",layer="data",driver="ESRI Shapefile")
   
