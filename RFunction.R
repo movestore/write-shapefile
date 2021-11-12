9fb38a21e304c45f9e10a0869b64d5d7d8c01ef0
Andrea Kölzsch <65662928+andreakoelzsch@users.noreply.github.com>
2021-11-12 14:10:06 +0100

    10 chars names

--- RFunction.R
+++ RFunction.R
@@ -12,6 +12,18 @@ rFunction <- function(data)
   targetDirShapeFiles <- paste0(targetDirZipFile,"/moveapps-shapefile")
   dir.create(targetDirShapeFiles)
   
+  # careful, for writeOGR the column names have to be unique in the first 10 characters, that can be problematic and has to be adressed here
+  for(i in seq(along=names(data.spdf@data)))
+  {
+    if(nchar(names(data.spdf@data)[i]) > 10) names(data.spdf@data)[i] <- substring(names(data.spdf@data)[i],1,9)
+  }
+  ix9 <- which(nchar(names(data.spdf@data))==9)
+  multi <- names(table(names(data.spdf@data)[ix9]))[table(names(data.spdf@data)[ix9])>1]
+  for (j in seq(along=multi))
+  {
+    ixmj <- which(names(data.spdf@data)==multi[j])
+    names(data.spdf@data)[ixmj] <- paste0(names(data.spdf@data)[ixmj],1:length(ixmj))
+  }
   writeOGR(data.spdf,dsn=targetDirShapeFiles,layer="data",driver="ESRI Shapefile",overwrite_layer=TRUE) 
   
   zip::zip(
